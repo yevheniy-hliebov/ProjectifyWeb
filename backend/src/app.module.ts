562c0cf84666597b1d244e581e9dafec6d05401e
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2023-11-24 13:51:27 +0200

    connect to mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
+import db_configuration from './config/database.configuration';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [configuration]
+      load: [configuration, db_configuration]
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('mongo_uri'), // Accessing mongolink from configuration
+      }),
     })
   ],
   controllers: [AppController],
---
+++ backend/src/config/database.configuration.ts
@@ -0,0 +1,3 @@
+export default () => ({
+    mongo_uri: process.env.MONGO_LINK,
+})
